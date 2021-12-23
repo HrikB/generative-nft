8566a89aed3d62d2919f0bba10026cc0efa7e25d
HrikB <hrik.bhowal@gmail.com>
2021-12-23 14:32:14 -0800

    Base Openzeppenlin contract

---
+++ contracts/ERC721/IERC721Receiver.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (token/ERC721/IERC721Receiver.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title ERC721 token receiver interface
+ * @dev Interface for any contract that wants to support safeTransfers
+ * from ERC721 asset contracts.
+ */
+interface IERC721Receiver {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}
